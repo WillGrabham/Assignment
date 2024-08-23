7b5f1f567995d46ac7a974faefd66fb1796caf25
Will Grabham <willifgr@amazon.com>
2024-08-23 13:25:41 +0100

    Add models for classes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,47 @@
 from app import db
 
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
+class Course(db.Model):
+    course_id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(80), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f"Student('{self.name}')"
+
+class Student(db.Model):
+    student_id = db.Column(db.Integer, primary_key=True)
+    student_email = db.Column(db.String(80), unique=True, nullable=False)
+    student_password = db.Column(db.String(80), unique=False, nullable=False)
+    student_name = db.Column(db.String(80), unique=False, nullable=False)
+    join_date = db.Column(db.Date, unique=False, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), unique=False, nullable=False)
+
+
+class Tutor(db.Model):
+    tutor_id = db.Column(db.Integer, primary_key=True)
+    tutor_name = db.Column(db.String(80), unique=False, nullable=False)
+    tutor_contact_email = db.Column(db.String(80), unique=False, nullable=False)
+    tutor_email = db.Column(db.String(80), unique=True, nullable=False)
+    tutor_password = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+
+class StudentTutor(db.Model):
+    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), primary_key=True)
+    tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.tutor_id'), primary_key=True)
+
+
+class ModuleEnrolment(db.Model):
+    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), primary_key=True)
+    module_id = db.Column(db.Integer, db.ForeignKey('module.module_id'), primary_key=True)
+    grade = db.Column(db.Integer, unique=False, nullable=True)
+    grade_date = db.Column(db.Date, unique=False, nullable=True)
+
+
+class Module(db.Model):
+    module_id = db.Column(db.Integer, primary_key=True)
+    module_name = db.Column(db.String(80), unique=False, nullable=False)
+
+
+class CourseModule(db.Model):
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), primary_key=True)
+    module_id = db.Column(db.Integer, db.ForeignKey('module.module_id'), primary_key=True)
+    module_order = db.Column(db.Integer, unique=False, nullable=False)
--- seed.py
+++ seed.py
@@ -1,10 +1,65 @@
+from datetime import datetime
+
+from flask_bcrypt import generate_password_hash
+
 from app import create_app, db
-from app.models import Student
+from app.models import Student, Course, Module, CourseModule, ModuleEnrolment, Tutor, StudentTutor
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-    student = Student(name="Some Name")
+
+    course = Course(course_name="Course name")
+    db.session.add(course)
+    db.session.commit()
+
+    new_course = Course.query.filter_by(course_name="Course name").first()
+    student = Student(
+        student_name="Student name",
+        student_email="student@email.com",
+        student_password=generate_password_hash('password').decode('utf-8'),
+        join_date=datetime.now().date(),
+        course_id=new_course.course_id)
     db.session.add(student)
+    db.session.commit()
+
+    module = Module(module_name="Module name")
+    db.session.add(module)
+    db.session.commit()
+
+    new_module = Module.query.filter_by(module_name="Module name").first()
+    courseModule = CourseModule(
+        course_id=new_course.course_id,
+        module_id=new_module.module_id,
+        module_order=1)
+    db.session.add(courseModule)
+    db.session.commit()
+
+    new_student = Student.query.filter_by(student_name="Student name").first()
+    moduleEnrolment = ModuleEnrolment(
+        student_id=new_student.student_id,
+        module_id=new_module.module_id,
+        grade=70,
+        grade_date=datetime.now().date()
+    )
+    db.session.add(moduleEnrolment)
+    db.session.commit()
+
+    tutor = Tutor(
+        tutor_name="Tutor name",
+        tutor_contact_email="some-email@domain.com",
+        tutor_email="admin@admin.com",
+        tutor_password=generate_password_hash('password').decode('utf-8'),
+        is_admin=True,
+    )
+    db.session.add(tutor)
+    db.session.commit()
+
+    new_tutor = Tutor.query.filter_by(tutor_name="Tutor name").first()
+    student_tutor = StudentTutor(
+        student_id=new_student.student_id,
+        tutor_id=new_tutor.tutor_id
+    )
+    db.session.add(student_tutor)
     db.session.commit()
