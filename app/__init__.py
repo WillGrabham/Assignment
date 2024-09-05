9abe03d139ed773dbcc632a4f03bd161d12eca5d
Will Grabham <willifgr@amazon.com>
2024-09-05 11:11:55 +0100

    Fix for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 
 db = SQLAlchemy()
+app = Flask(__name__)
 
 
 def create_app():
-    app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     from app.routes import main
